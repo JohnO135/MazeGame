8344ce8e12538442a9fdc9f4f445b1e98e4cf6c2
JohnO135 <john.ong135@gmail.com>
2019-09-09 15:41:07 -0700

    Set the background of panel to Green

--- src/GameSetup/Border.java
+++ src/GameSetup/Border.java
@@ -3,15 +3,20 @@ package GameSetup;
 
 import javax.swing.JFrame;
 
-public class Border {
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class Border{
 	
 	public Border() {
 		JFrame mainWindow = new JFrame("Lettuce Adventure: Grilled Redux");
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setPreferredSize(new Dimension(800,500));
+		mainWindow.setLocationRelativeTo(null);
+		mainWindow.getContentPane().setBackground(Color.GREEN);
 		mainWindow.pack();
 		mainWindow.setVisible(true);
-		mainWindow.setSize(800, 500);
-		mainWindow.setLocationRelativeTo(null);
+		
 	}
 
 }
